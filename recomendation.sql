c6944a1d77673d47780ed8d3f7010ceae64b3720
MacarenaFF <95181093+MacarenaFF@users.noreply.github.com>
2022-10-03 16:09:29 -0300

    Add files via upload

---
+++ recomendation.sql
@@ -0,0 +1,8 @@
+use crud_nodes_db;
+INSERT INTO `recomendation` (`Id_usuario`, `name`, `lastname`, `company`, `recomendation`) VALUES
+(1, 'Macarena', 'Fernandez', 'Amdocs', 'Los invito a qu dejen su comentario!!'),
+(2, 'Sergio ', 'Bollati', 'Claro', 'Muy buena! a seguir sumando...'),
+(3, 'Ariel', 'VDKP', 'Nokia', 'Responsable, criteriosa, ojala nos volvamoes acruzar'),
+(4, 'Edgardo', 'Ramos', 'Nokia', 'Necesito ayuda con mi página! avisame cuando podemos verlo :) ')
+--
+-- Índices para tablas volcadas
